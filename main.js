4241f28cc68df45543a5a5ad7256eb799648ead6
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-07-25 08:00:36 +0530

    add max wins logic

--- main.js
+++ main.js
@@ -9,58 +9,55 @@ let rank = {
   3: "rock",
 };
 
+let gameCount = 0;
+
 function getComputerChoice() {
   let randInt = Math.floor(Math.random() * 3) + 1;
   return rank[randInt];
 }
 
 function playRound(playerSelection) {
-  // if (playerSelection >= 1 && playerSelection <= 3) {
-  let computerSelection = getComputerChoice();
-  // if (playerSelection > computerSelection) {
-  //   score.player += 1;
-  // } else if (playerSelection < computerSelection) {
-  //   score.computer += 1;
-  // } else if (playerSelection === computerSelection) {
-  //   score.player += 1;
-  //   score.computer += 1;
-  // }
-  // console.log(
-  //   `Player: ${rank[playerSelection]} <<>> Computer: ${rank[computerSelection]}`
-  // );
-  // return score;
-
-  if (playerSelection === "paper") {
-    if (computerSelection === "rock") {
-      score.player += 1;
-      return "paper wins";
-    } else {
-      if (computerSelection === "scissors") {
-        score.computer += 1;
-        return "scissors wins";
-      }
-    }
-  }
-  if (playerSelection === "scissors") {
-    if (computerSelection === "rock") {
-      score.computer += 1;
-      return "rock wins";
-    } else {
-      if (computerSelection === "paper") {
+  if (gameCount < 5) {
+    gameCount++;
+    if (playerSelection === "paper") {
+      if (computerSelection === "rock") {
         score.player += 1;
-        return "scissors wins";
-      }
-    }
-  }
-  if (playerSelection === "rock") {
-    if (computerSelection == "scissors") {
-      score.player += 1;
-      return "rock wins";
-    } else {
-      if (computerSelection === "paper") {
-        score.computer += 1;
         return "paper wins";
+      } else {
+        if (computerSelection === "scissors") {
+          score.computer += 1;
+          return "scissors wins";
+        }
       }
     }
+    if (playerSelection === "scissors") {
+      if (computerSelection === "rock") {
+        score.computer += 1;
+        return "rock wins";
+      } else {
+        if (computerSelection === "paper") {
+          score.player += 1;
+          return "scissors wins";
+        }
+      }
+    }
+    if (playerSelection === "rock") {
+      if (computerSelection == "scissors") {
+        score.player += 1;
+        return "rock wins";
+      } else {
+        if (computerSelection === "paper") {
+          score.computer += 1;
+          return "paper wins";
+        }
+      }
+    }
+  } else {
+    // return the winner with max wins
+    gameCount = 0;
+    score.player = 0;
+    score.computer = 0;
   }
+
+  let computerSelection = getComputerChoice();
 }
